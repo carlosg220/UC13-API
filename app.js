cb7cd064aadae8a4ad2e8630e408ce9938ad8d09
Carlos <carlosgabrielsilvagomes1@gmail.com>
2025-01-22 17:00:35 -0300

    adicionado json

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+import carros2024 from './tabelaCarros.js';
+
+// Import o modulo do Express
 import express from 'express';
 
 const app = express();
 
-//Define a porta do servidor
+app.use(express.json());
+
+app.get('/', (requisicao, resposta) => {
+    resposta.status(200).send(carros2024); // Retorna lista de carro com staus 200
+});
+
+// Define a porta do servidor
 app.listen(3000, () => {
     console.log('Servidor rodando com sucesso na porta 3000')
 });
---
+++ tabelaCarros.js
@@ -0,0 +1,24 @@
+const carros2024 = [
+    {
+        nome: 'Ferrari',
+        sigla: 'FER',
+        velocidadeMaxima: 340,
+        potencia: 800,
+        consumo: 5.5,
+        aceleracao: 2.9,
+        preco: 30000000
+    },
+
+    {
+        nome: 'Lamborghini',
+        sigla: 'LAM',
+        velocidadeMaxima: 355,
+        potencia: 770,
+        consumo: 6.2,
+        aceleracao: 2.8,
+        preco: 50000000
+    }
+
+]
+
+export default carros2024;
